814581f85232d6a728b62b1e24783ac9ccd638b3
Inna Yasynchuk <i_bybyk@ukr.net>
2023-04-01 14:23:31 +0300

    made burger-menu

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,36 +1,43 @@
-import React from "react";
+import React, {useState} from "react";
 import { NavLink } from "react-router-dom";
 import "./Navbar.scss";
-import { Logo, H, Y, D, R, A  } from "../svg";
+import { Logo, LogoMini, H, Y, D, R, A, HMini, YMini, DMini, RMini, AMini } from "../svg";
 
 
 const Navbar = () => {
+
+const[burger, setBurger] = useState(false);
+  
+const isMobile = window.innerWidth <= 600;
+  
   return (
-    <header className="header container">
+    <header className="header">
       <div className="header-logo">
       <NavLink to='/'>       
-          <span className="header-logo__item-img"><Logo /></span>
+          <span className="header-logo__item-img">{isMobile ? <LogoMini /> : <Logo />}</span>
       </NavLink> 
       <NavLink to='/'>   
-          <span className="header-logo__item"><H /></span>
-          <span className="header-logo__item"><Y /></span>
-          <span className="header-logo__item"><D /></span>
-          <span className="header-logo__item"><R /></span>
-          <span className="header-logo__item"><A /></span>
+          <span className="header-logo__item">{isMobile ? <HMini /> : <H />}</span>
+          <span className="header-logo__item">{isMobile ? <YMini /> : <Y />}</span>
+          <span className="header-logo__item">{isMobile ? <DMini /> : <D />}</span>
+          <span className="header-logo__item">{isMobile ? <RMini /> : <R />}</span>
+          <span className="header-logo__item">{isMobile ? <AMini /> : <A />}</span>
       </NavLink>
       </div>
       <nav className="header-menu">
-        <ul className="header-menu-list">
-          <li><NavLink to='/about'className={({isActive}) => isActive ? "header-menu__link__active" : "header-menu__link"} >About</NavLink></li>
-          <li><NavLink to='/services' className={({isActive}) => isActive ? "header-menu__link__active" : "header-menu__link"} >Services</NavLink></li>
-          <li><NavLink to='/technologies' className={({isActive}) => isActive ? "header-menu__link__active" : "header-menu__link"} >Technologies</NavLink></li>
-          <li><NavLink to='/how_to' className={({isActive}) => isActive ? "header-menu__link__active" : "header-menu__link"} >How to</NavLink></li>
+        <ul className={ burger ? "header-menu-list_active" : "header-menu-list"} >
+          <li><NavLink to='/about'className={({isActive}) => isActive ? "header-menu__link__active" : "header-menu__link"} onClick={() =>{ if(isMobile) {setBurger(!burger)}}} >About</NavLink></li>
+          <li><NavLink to='/services' className={({isActive}) => isActive ? "header-menu__link__active" : "header-menu__link" } onClick={() =>{ if(isMobile) {setBurger(!burger)}}} >Services</NavLink></li>
+          <li><NavLink to='/technologies' className={({isActive}) => isActive ? "header-menu__link__active" : "header-menu__link"} onClick={() =>{ if(isMobile) {setBurger(!burger)}}} >Technologies</NavLink></li>
+          <li><NavLink to='/how_to' className={({isActive}) => isActive ? "header-menu__link__active" : "header-menu__link"} onClick={() =>{ if(isMobile) {setBurger(!burger)}}} >How to</NavLink></li>
         </ul>
       </nav>
         <ul className="navbar-button">
           <li><NavLink to='/contact' className="navbar-button__button">Contact us</NavLink></li>
           <li><NavLink to='/join' className="navbar-button__button">Join Gidra</NavLink></li>
         </ul>
+        <button onClick={() => setBurger(!burger)} className={burger ? "burger-menu_open" : "burger-menu" } >
+          <span></span></button>
     </header>
   );
 };
