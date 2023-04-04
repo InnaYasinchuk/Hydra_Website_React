d5d679cfb85923ed9e87a95793b4fdb033ae2451
Inna Yasynchuk <i_bybyk@ukr.net>
2023-04-04 22:46:23 +0300

    made desktop home-page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,27 +1,43 @@
-import React from 'react';
+import React from "react";
 import { NavLink } from "react-router-dom";
 import "./Home.scss";
 
-import { HomeV1, HomeV2, HomeV3 } from './svg';
-import mainImage from '../../pages/Home/images/home-img.webp';
-import Slider from '../../components/Slider/Slider';
+import { HomeV1, HomeV2, HomeV3, HomeV4 } from "./svg";
+import mainImage from "../../pages/Home/images/home-img.webp";
+import arrowRight from "../../pages/Home/images/home-arrow-small-right1.png"
+import Slider from "../../components/Slider/Slider";
 
 const Home = () => {
-  return ( 
-  
-  <div className='home-page'>
-   
-    <div className='home-page__border'>
-       <img className='home-page__image' src={mainImage} alt='virtual'></img>
-        <svg className="home-page_vector1"><HomeV1 /></svg>
-       <svg className="home-page_vector2"><HomeV2 /></svg>
-       <svg className="home-page_vector3"><HomeV3 /></svg>
+  const isMobile = window.innerWidth <= 768;
+  return (
+    <div className="home">
+      <section className="home-page">
+        <div className="home-page__border">
+          <img className="home-page__image" src={mainImage} alt="virtual"></img>
+          <svg className="home-page_vector1"><HomeV1 /></svg>
+          <svg className="home-page_vector2"><HomeV2 /></svg>
+          <svg className="home-page_vector3"><HomeV3 /></svg>
+        </div>
+        <div className="home-page__description">
+          <svg className="home-page_vector4"><HomeV4 /></svg>
+          <h1 className="home-page__title"><span className="home-page__title_accent1">Dive</span> Into The
+            Depths <br /> Of<span className="home-page__title_accent2"> Virtual Reality</span>
+          </h1>
+          { isMobile ? '' : <p className="home-page__text">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore nisl tincidunt eget.
+              Lectus mauris eros in vitae.
+            </p>
+          }
+          <NavLink to="/services" className="home-page__button">
+            Build your world
+          </NavLink>
+          <img src={arrowRight} alt="arrow" className="home-page__arrow"></img>
+        </div>
+      </section>
+      <Slider />
     </div>
-    
-    <h1 className='home-page__title'> <span className='home-page__title_accent1'>Dive</span> Into The Depths <br/> Of <span className='home-page__title_accent2'> Virtual Reality</span></h1>
-    <NavLink to='/services' className="home-page__button">Build your world</NavLink>
-   <Slider />
-  </div> );
-}
- 
+  );
+};
+
 export default Home;
