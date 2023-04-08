b43b0a53766011ff107512549a87955983c400b9
Inna Yasynchuk <i_bybyk@ukr.net>
2023-04-08 20:21:25 +0300

    made about-section

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -3,26 +3,31 @@ import { NavLink } from "react-router-dom";
 import "./About.scss";
 
 import aboutImg from "./images/about-img.webp";
+import { AboutV7, AboutV8, AboutString, AboutArrow } from "./svg";
 
 const About = () => {
   return (
     <section className="about">
-      <div className="about-titles">
-        <h1 className="about-titles__title">INTRODUCTION</h1>
-        <h2 className="about-titles__subtitle">TO HYDRA VR</h2>
-        <img src={aboutImg} alt="man" className="about__img"></img>
+      <div className="about-first-blok">
+        <h1 className="about-first-blok__title">INTRODUCTION</h1>
+        <h2 className="about-first-blok__subtitle">TO HYDRA VR</h2>
+        <img src={aboutImg} alt="man" className="about-first-blok__img"></img>
+        <svg className="about-first-blok__vector7"><AboutV7 /></svg>
+        <svg className="about-first-blok__vector8"><AboutV8 /></svg>
+        <svg className="about-first-blok__vectorString"><AboutString /></svg>
+        <svg className="about-first-blok__vectorArrow"><AboutArrow /></svg>
       </div>
-      <div>
-        <p className="about__description_top">
+      <div className="about-second-blok">
+        <p className="about-second-blok__description_top">
           Vitae sapien pellentesque habitant morbi tristique senectus et netus
           et. Feugiat nibh sed pulvinar proin gravida hendrerit lectus. Mi sit
           amet mauris commodo quis imperdiet massa tincidunt nunc. Viverra
           aliquet eget sit amet tellus. Ornare lectus sit amet est placerat in.
           Lectus magna fringilla urna porttitor rhoncus vitae.
         </p>
-        <h1 className="about-titles__title">ABOUT</h1>
-        <h2 className="about-titles__subtitle">HYDRA VR</h2>
-        <p className="about__description">
+        <h1 className="about-second-blok__title">ABOUT</h1>
+        <h2 className="about-second-blok__subtitle">HYDRA VR</h2>
+        <p className="about-second-blok__description">
           Eget mi proin sed libero enim sed faucibus turpis. Nisl rhoncus mattis
           rhoncus urna neque viverra justo. Vivamus at augue eget arcu dictum.
           Ultrices gravida dictum fusce ut placerat orci. Aenean et tortor at
@@ -34,8 +39,8 @@ const About = () => {
           diam sit amet nisl suscipit adipiscing bibendum. Leo a diam
           sollicitudi n tempor.
         </p>
-        <NavLink to="/join" className="about__button">
-          Let`s get in touch
+        <NavLink to="/join" className="about-second-blok__button">
+          Let's get in touch
         </NavLink>
       </div>
     </section>
---
+++ src/pages/About/svg.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+export const AboutV7 = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="1440" height="262" viewBox="0 0 1440 262" fill="none">
+  <path d="M1442.5 215C1387.5 104.333 1242.8 -83.7999 1104 49.0001C930.5 215 761.5 331.5 554.5 203.5C347.5 75.5001 34.5 175.5 -6.5 260" stroke="url(#paint0_linear_3_53)" strokeOpacity="0.13" strokeWidth="6"/>
+  <defs>
+  <linearGradient id="paint0_linear_3_53" x1="-7" y1="105.5" x2="1443" y2="89.0001" gradientUnits="userSpaceOnUse">
+  <stop stopColor="#211E2E"/>
+  <stop offset="0.3125" stopColor="#C0B7E8"/>
+  <stop offset="0.765625" stopColor="#8176AF"/>
+  <stop offset="0.994792" stopColor="#211E2E"/>
+  </linearGradient>
+  </defs>
+  </svg>
+)
+
+export const AboutV8 = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="1440" height="219" viewBox="0 0 1440 219" fill="none">
+  <path d="M-4.5 74.5C153.667 21.1667 504.6 -53.5 643 74.5C816 234.5 1071.5 258.5 1186.5 146C1278.5 56 1394.17 70.8333 1440.5 89.5" stroke="url(#paint0_linear_3_54)" strokeOpacity="0.13" strokeWidth="6"/>
+  <defs>
+  <linearGradient id="paint0_linear_3_54" x1="-5" y1="63" x2="1462" y2="62.9999" gradientUnits="userSpaceOnUse">
+  <stop stopColor="#343045"/>
+  <stop offset="0.224242" stopColor="#C0B7E8"/>
+  <stop offset="0.786458" stopColor="#8176AF"/>
+  <stop offset="0.96875" stopColor="#343045"/>
+  </linearGradient>
+  </defs>
+  </svg>
+)
+
+export const AboutString = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="158" height="4" viewBox="0 0 158 4" fill="none">
+  <path d="M157.5 2H0" stroke="#C0B7E8" strokeWidth="3"/>
+  </svg>
+)
+
+export const AboutArrow = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="46" height="38" viewBox="0 0 46 38" fill="none">
+  <path d="M43.8333 19L2.16667 19M43.8333 19L27.1667 35.6666M43.8333 19L27.1667 2.33331" stroke="#C0B7E8" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round"/>
+  </svg>
+)
+
+
